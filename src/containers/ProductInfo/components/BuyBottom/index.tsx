c42c06e20e4359df1c095ce5a54a3fe7bb89b760
Blackstone <601735416@qq.com>
2023-08-11 00:00:00 +0800

    feat: 🎸 添加购买信息页面

--- src/containers/ProductInfo/components/BuyBottom/index.tsx
+++ src/containers/ProductInfo/components/BuyBottom/index.tsx
@@ -1,6 +1,8 @@
 import { IProduct } from '@/utils/types';
 import { Grid } from 'antd-mobile';
 import { PhoneFill } from 'antd-mobile-icons';
+import { useGoTo } from '@/hooks';
+import { ROUTE_KEY } from '@/routes/menus';
 import style from './index.module.less';
 
 interface IProps {
@@ -11,30 +13,39 @@ interface IProps {
 */
 const BuyBottom = ({
   data,
-}: IProps) => (
-  <Grid columns={10} className={style.container}>
-    <Grid.Item span={4}>
-      <span className={style.preferentialPrice}>
-        ￥
-        {data.preferentialPrice}
-      </span>
-      <span className={style.originalPrice}>
-        ￥
-        {data.originalPrice}
-      </span>
-    </Grid.Item>
-    <Grid.Item span={2}>
-      <a href={`tel:${data.org.tel}`}>
-        <PhoneFill className={style.tel} />
-      </a>
-    </Grid.Item>
-    <Grid.Item
-      span={4}
-      className={style.buyButton}
-    >
-      立即抢购
-    </Grid.Item>
-  </Grid>
-);
+}: IProps) => {
+  const { go } = useGoTo();
+  const goBuy = () => {
+    go(ROUTE_KEY.BUY, {
+      id: data.id,
+    });
+  };
+  return (
+    <Grid columns={10} className={style.container}>
+      <Grid.Item span={4}>
+        <span className={style.preferentialPrice}>
+          ￥
+          {data.preferentialPrice}
+        </span>
+        <span className={style.originalPrice}>
+          ￥
+          {data.originalPrice}
+        </span>
+      </Grid.Item>
+      <Grid.Item span={2}>
+        <a href={`tel:${data.org.tel}`}>
+          <PhoneFill className={style.tel} />
+        </a>
+      </Grid.Item>
+      <Grid.Item
+        span={4}
+        className={style.buyButton}
+        onClick={goBuy}
+      >
+        立即抢购
+      </Grid.Item>
+    </Grid>
+  );
+};
 
 export default BuyBottom;
