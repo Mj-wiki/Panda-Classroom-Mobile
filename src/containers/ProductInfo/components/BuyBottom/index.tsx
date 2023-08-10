0757344727f69b68ba74ca0d6c8102e608c50037
Blackstone <601735416@qq.com>
2023-08-11 00:00:00 +0800

    feat: 🎸 编写 H5 的商品详情页面

--- src/containers/ProductInfo/components/BuyBottom/index.tsx
+++ src/containers/ProductInfo/components/BuyBottom/index.tsx
@@ -1,7 +1,6 @@
 import { IProduct } from '@/utils/types';
 import { Grid } from 'antd-mobile';
 import { PhoneFill } from 'antd-mobile-icons';
-import { useUserContext } from '@/hooks/userHooks';
 import style from './index.module.less';
 
 interface IProps {
@@ -12,34 +11,30 @@ interface IProps {
 */
 const BuyBottom = ({
   data,
-}: IProps) => {
-  const { store } = useUserContext();
-
-  return (
-    <Grid columns={10} className={style.container}>
-      <Grid.Item span={4}>
-        <span className={style.preferentialPrice}>
-          ￥
-          {data.preferentialPrice}
-        </span>
-        <span className={style.originalPrice}>
-          ￥
-          {data.originalPrice}
-        </span>
-      </Grid.Item>
-      <Grid.Item span={2}>
-        <a href={`tel:${store.tel}`}>
-          <PhoneFill className={style.tel} />
-        </a>
-      </Grid.Item>
-      <Grid.Item
-        span={4}
-        className={style.buyButton}
-      >
-        立即抢购
-      </Grid.Item>
-    </Grid>
-  );
-};
+}: IProps) => (
+  <Grid columns={10} className={style.container}>
+    <Grid.Item span={4}>
+      <span className={style.preferentialPrice}>
+        ￥
+        {data.preferentialPrice}
+      </span>
+      <span className={style.originalPrice}>
+        ￥
+        {data.originalPrice}
+      </span>
+    </Grid.Item>
+    <Grid.Item span={2}>
+      <a href={`tel:${data.org.tel}`}>
+        <PhoneFill className={style.tel} />
+      </a>
+    </Grid.Item>
+    <Grid.Item
+      span={4}
+      className={style.buyButton}
+    >
+      立即抢购
+    </Grid.Item>
+  </Grid>
+);
 
 export default BuyBottom;
